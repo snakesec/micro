9feeb20a04d6b0197f4edbc19b238e369cc7f685
Weidsom Nascimento <weidsom@snakesecurity.org>
2026-09-15 21:15:48 -0300

    Update micro installer to 2.0.15

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -1,17 +1,11 @@
-make build
-
-if [ $? -eq 0 ]
-then
-  # Result is OK! Just continue...
-  echo "Make build... PASS!"
-else
-  # houston we have a problem
-  exit 1
-fi
 
 strip micro
 
-cp -Rf micro /opt/ANDRAX/bin
+if [ $(uname -m | grep 'x86_64') ]; then
+  cp -Rf micro-x86_64 /opt/ANDRAX/bin/
+else
+  cp -Rf micro-aarch64 /opt/ANDRAX/bin/
+fi
 
 if [ $? -eq 0 ]
 then
@@ -21,3 +15,30 @@ else
   # houston we have a problem
   exit 1
 fi
+
+source /opt/AFOS/afos_shell_functions.sh
+
+check_package_installed_apt "man-db"
+
+if [ $? -eq 1 ]
+then
+  # Result is OK! Just continue...
+  echo "Man already installed... PASS!"
+else
+  # Install man-db using apt
+  apt update
+  apt install --no-install-recommends -y man-db
+fi
+
+cp micro.1 /usr/local/share/man/man1/
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Copy man file... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+mandb
--- internal/action/actions_other.go
+++
@@ -1,8 +0,0 @@
-//go:build plan9 || nacl || windows
-
-package action
-
-func (*BufPane) Suspend() bool {
-	InfoBar.Error("Suspend is only supported on BSD/Linux")
-	return false
-}
